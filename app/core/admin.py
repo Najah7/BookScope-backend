047bf0f94823eae05e375789f6532171254aae20
Najah7 <najahmatsuo7@gmail.com>
2023-05-15 23:47:48 +0900

    🐛 手違いで消してしまったcoreアプリのファイル類を追加

---
+++ app/core/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+
+from core import models
+
+# Register your models here.
+
+admin.site.register(models.CustomUser)
+admin.site.register(models.FavoritePhrase)
+admin.site.register(models.FavoritePhraseLike)
+admin.site.register(models.Major)
+admin.site.register(models.Rank)
+admin.site.register(models.School)
+admin.site.register(models.WroteHistory)
+admin.site.register(models.ReadBook)
+admin.site.register(models.Book)
+admin.site.register(models.Author)
+admin.site.register(models.BookTag)
+admin.site.register(models.Publisher)
+admin.site.register(models.Post)
+admin.site.register(models.PostComment)
+admin.site.register(models.PostLike)
+
+
+
+
