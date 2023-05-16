9fa315d788c797bcf1be5042f0484249670db9ad
Najah7 <najahmatsuo7@gmail.com>
2023-05-17 01:29:18 +0900

    🐛 #1ER図を見て気づいたRelationの実装ミスを修正

--- app/core/models/read_book.py
+++ app/core/models/read_book.py
@@ -1,15 +1,16 @@
 from django.db import models
-
 from django.conf import settings
 
+from core.models.book import Book
+
+
 class ReadBook(models.Model):
-    
     class Meta:
-        db_table = 'read_books'
+        db_table = "read_books"
         app_label = "core"
-    
+
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    book_id = models.IntegerField()
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
     read_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
