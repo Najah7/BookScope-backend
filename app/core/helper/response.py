04793f8060bb18f0757a288dbf7c8f0f12746323
Najah7 <najahmatsuo7@gmail.com>
2023-05-21 10:17:32 +0900

    ✨ #26 ヘルパー関数を追加

--- app/core/helper/response.py
+++ app/core/helper/response.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 
-def generate_book_response(book):
+
+def generate_book_response_from_obj(book):
     return Response({
         "book_id": book.id,
         "title": book.title,
@@ -12,7 +13,7 @@ def generate_book_response(book):
         "tags": book.tags,
         })
 
-def generate_book_responses(books):
+def generate_book_responses_from_objs(books):
     res_books = []
     for book in books:
         res_book = {
@@ -27,7 +28,23 @@ def generate_book_responses(books):
         }
         res_books.append(res_book)
     return Response(res_books)
+
+def format_api_response(api_res):
+    book_info = api_res["Items"][0]["Item"]
+    title = book_info["title"]
+    author = book_info["author"]
+    publisher = book_info["publisherName"]
+    price = book_info["itemPrice"]
+    image_url = book_info["largeImageUrl"]
     
+    return {
+        "title": title,
+        "author": author,
+        "publisher": publisher,
+        "price": price,
+        "image_url": image_url
+    }
+
 def wrong_isbn_response():
     return Response({
         "message": "本が見つかりませんでした。",
